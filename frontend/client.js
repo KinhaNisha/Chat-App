0ac13e59c020862a41983c602ed4c25dd12a2b0c
KinhaNisha <nishakinha1411@gmail.com>
2024-05-02 20:16:13 +0530

    Add sound when message is send or received

--- frontend/client.js
+++ frontend/client.js
@@ -3,6 +3,8 @@ const socket = io('http://localhost:3000');
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
+const sound = new Audio('sound.mp3')
+
 
 const appendMessage = (message, position) => {
     const messageElement = document.createElement('div');
@@ -10,6 +12,10 @@ const appendMessage = (message, position) => {
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
+}
+
+const playSound = () => {
+    sound.play();
 }
 
 const Username = prompt("Enter your name to join chat");
@@ -20,11 +26,13 @@ socket.emit('new-user-joined', Username);
 // Listen for new user joined event
 socket.on('user-joined', data => {
     appendMessage(`${data} joined the chat`, 'right');
+    playSound();
 });
 
 // Listen for receive message event
 socket.on('receive-message', data => {
     appendMessage(`${data.name}: ${data.message}`, 'left');
+    playSound();
 });
 
 // Send message when form is submitted
